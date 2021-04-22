cfb64d8d99b63e523fbb9614d64675c83d4c4fd1
Felix-Antoine Fortin <felix@calculquebec.ca>
2021-04-22 18:30:13 +0000

    Add test for k8s

--- Puppetfile
+++ Puppetfile
@@ -3,3 +3,5 @@
 forge "https://forgeapi.puppetlabs.com"
 
 mod 'puppetlabs-stdlib', '5.2.0'
+mod 'puppet-selinux', '3.2.0'
+mod 'puppetlabs-kubernetes', '6.0.0'
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,13 @@
-node default {
+node master1 {
+  package {'vim':}
 
+  class { 'selinux': 
+    mode => 'disabled'
+  }
+
+  class { 'kubernetes': 
+    controller => true
+    require    => Class['selinux']
+  }
 }
 
