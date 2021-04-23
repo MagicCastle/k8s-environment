2b643cd1d3853de6dd7520ffe93286788e63103c
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-04-22 20:53:37 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,9 +5,22 @@ node /master\d+/ {
     mode => 'disabled'
   }
 
+  $instances = lookup('terraform.instances')
+$host_template = @(END)
+127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
+<% @instances.each do |key, values| -%>
+<%= values['local_ip'] %> <%= key %> <% if values['tags'].include?('puppet') %>puppet<% end %>
+<% end -%>
+END
+
+  file { '/etc/hosts':
+    ensure  => file,
+    content => inline_template($host_template)
+  }
+
   class { 'kubernetes': 
     controller => true,
-    require    => Class['selinux']
+    require    => [Class['selinux'], File['/etc/hosts']]
   }
 }
 
@@ -18,8 +31,21 @@ node default {
     mode => 'disabled'
   }
 
+  $instances = lookup('terraform.instances')
+$host_template = @(END)
+127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
+<% @instances.each do |key, values| -%>
+<%= values['local_ip'] %> <%= key %> <% if values['tags'].include?('puppet') %>puppet<% end %>
+<% end -%>
+END
+
+  file { '/etc/hosts':
+    ensure  => file,
+    content => inline_template($host_template)
+  }
+
   class { 'kubernetes': 
-    worker => true,
-    require    => Class['selinux']
+    worker   => true,
+    require  => [Class['selinux'], File['/etc/hosts']]
   }
 }
