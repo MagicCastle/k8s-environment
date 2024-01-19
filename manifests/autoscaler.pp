1465f147e14dca06a60631f632cda6de4d5807d2
Etienne <>
2024-01-18 22:04:45 -0500

    Fix small mistakes

--- manifests/autoscaler.pp
+++ manifests/autoscaler.pp
@@ -38,7 +38,7 @@ class autoscaler ()
   exec { 'autoscaler_config':
     command     => 'kubectl create cm externalgrpc-autoscaler-cluster-config --from-file=autoscaler.yaml --namespace=kube-system -o yaml',
     path        => ['/usr/bin'],
-    cwd         => ['/etc/kubernetes/'],
+    cwd         => '/etc/kubernetes/',
     environment => ['KUBECONFIG=/etc/kubernetes/admin.conf'],
     refreshonly => true,
     tries       => 4,
@@ -52,7 +52,7 @@ class autoscaler ()
   exec { 'autoscaler_deploy':
     command     => 'kubectl apply -f cluster-autoscaler-config.yaml -f ./cluster-autoscaler.yaml -f ./externalgrpc-autoscaler-service.yaml -f ./externalgrpc-autoscaler.yaml',
     path        => ['/usr/bin'],
-    cwd         => ['/etc/kubernetes/k8s-autoscaler/deploy'],
+    cwd         => '/etc/kubernetes/k8s-autoscaler/deploy',
     environment => ['KUBECONFIG=/etc/kubernetes/admin.conf'],
     refreshonly => true,
     tries       => 4,
@@ -60,7 +60,7 @@ class autoscaler ()
     timeout     => 15,
     require     => [
       Exec['autoscaler_config'],
-      File['/etc/kubernetes/k8s-autoscaler'],
+      Vcsrepo['/etc/kubernetes/k8s-autoscaler'],
       File['/etc/kubernetes/autoscaler.yaml']
     ],
   }
